b94cd71c99b610a969f5c3439ec209da60640e87
Bryan James Bigej <bbigej@gmail.com>
2017-08-16 23:34:04 -0700

    implemented second list

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 var locMessage = "";
 var message = "";
+var locMessage2 = "";
+var message2 = "";
+
+var hoursArray = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",]
 
 function updatePage() {
     document.getElementById('cookieStoreLoc').innerHTML = locMessage;
     document.getElementById('cookieStore').innerHTML = message;
+    document.getElementById('cookieStoreLoc2').innerHTML = locMessage2;
+    document.getElementById('cookieStore2').innerHTML = message2;   
 }
 
-var hoursArray = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",]
 
 var pioneerSq = {
     storeLoc: "Pioneer Square",
@@ -34,41 +39,50 @@ pioneerSq.rndNumCustHr();
 
 
 var pdxAirport = {
+    storeLoc: "Portland Airport",
     minCustHr: 6,
-    MaxCustHr: 24,
+    maxCustHr: 24,
     avgCookiePerCust: 1.2,
-    randCustPerHr: function() {
-        math.random() * (maxCustHr - minCustHr) + minCustHr;  
-        return custNum;
+    rndNumCustHr: function() {
+        var totalCookies = 0;
+        var cookieArray = new Array();
+        locMessage2 = pdxAirport.storeLoc
+        for (var index = 0; index < 8; index++) {
+            var custNum = Math.floor(Math.random() * (pdxAirport.maxCustHr - pdxAirport.minCustHr) + pdxAirport.minCustHr);
+            var cookiesThatHr = custNum * pdxAirport.avgCookiePerCust;
+            cookieArray.push(cookiesThatHr);
+            totalCookies = totalCookies + Math.floor(cookiesThatHr);
+            message2 += "<li>" + hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies </li>";
+        }
+        message2 += "<li>Total: " + totalCookies + " cookies </li>";
+        updatePage();     
     },
 };
+
+pdxAirport.rndNumCustHr();
     
+
+
+
+
+
 var WashSq = {
     minCustHr: 11,
-    MaxCustHr: 38,
+    maxCustHr: 38,
     avgCookiePerCust: 1.9,
-    randCustPerHr: function() {
-        math.random() * (maxCustHr - minCustHr) + minCustHr; 
-        return custNum;
-    },
+   
 };
 
 var sellwood = {
     minCustHr: 20, 
-    MaxCustHr: 48,
+    maxCustHr: 48,
     avgCookiePerCust: 3.3,
-    randCustPerHr: function() {
-        math.random() * (maxCustHr - minCustHr) + minCustHr;
-        return custNum;
-    },
+  
 };  
 
 var pearlDis = {
     minCustHr: 3,
-    MaxCustHr: 24, 
+    maxCustHr: 24, 
     avgCookiePerCust: 2.6,
-    randCustPerHr: function() {
-        math.random() * (maxCustHr - minCustHr) + minCustHr;
-        return custNum;
-    },
+   
 };
