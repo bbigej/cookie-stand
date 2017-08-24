3fe225e16c7c9b8f084f73e8fffed1792e0f73e6
Bryan James Bigej <bbigej@gmail.com>
2017-08-23 20:31:20 -0700

    all 5 tables loaded

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var Store = function(name, minCustHr, maxCustHr, avgCookiePerCust) {
         nameCell.innerText = this.storeName;
         nameRow.appendChild(nameCell);
         tableReference.appendChild(nameRow);
-      //  return nameRow
 
         var totalCookies = 0;
         var cookieArray = new Array();
@@ -29,7 +28,6 @@ var Store = function(name, minCustHr, maxCustHr, avgCookiePerCust) {
             cookieHrCell.innerText = hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies";
             cookieHrRow.appendChild(cookieHrCell);
             tableReference.appendChild(cookieHrRow);
-           // return cookieHrRow
             }
 
         var totalRow = document.createElement("tr");
@@ -43,6 +41,10 @@ var Store = function(name, minCustHr, maxCustHr, avgCookiePerCust) {
 
 var storeRoster = [];
 storeRoster.push(new Store("Pioneer Square", 17, 88, 5.2));
+storeRoster.push(new Store("Portland Airport", 6, 24, 1.2));
+storeRoster.push(new Store("Washington Square",	11, 38,	1.9));
+storeRoster.push(new Store("Sellwood", 20, 48, 3.3));
+storeRoster.push(new Store("Pearl District", 3, 24, 2.6));
 
 function buildStoreTable() {
     var tableBody = document.getElementById("table-body");
@@ -52,150 +54,3 @@ function buildStoreTable() {
 }
 
 buildStoreTable();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* var hoursArray = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",]
-
- function addStoreName(addedStoreName, storeNameId) {
-    var pGraphToUse = document.getElementById(storeNameId);
-    pGraphToUse.innerText = addedStoreName;
-  }
-
-function addListItem(contentToAdd, listId) {
-    var listToUse = document.getElementById(listId);
-    var cookieStandItem = document.createElement("li");
-    cookieStandItem.setAttribute('class', 'data')
-    cookieStandItem.innerText = contentToAdd;
-    listToUse.appendChild(cookieStandItem);
-  }
-
-var pioneerSq = {
-    storeLoc: "Pioneer Square",
-    minCustHr: 17, 
-    maxCustHr: 88,
-    avgCookiePerCust: 5.2,
-    rndNumCustHr: function() {
-        var totalCookies = 0;
-        var cookieArray = new Array();
-        addStoreName(pioneerSq.storeLoc, "cookieStoreLoc");
-        for (var index = 0; index < 8; index++) {
-            var custNum = Math.floor(Math.random() * (pioneerSq.maxCustHr - pioneerSq.minCustHr) + pioneerSq.minCustHr);
-            var cookiesThatHr = custNum * pioneerSq.avgCookiePerCust;
-            cookieArray.push(cookiesThatHr);
-            totalCookies = totalCookies + Math.floor(cookiesThatHr);
-            addListItem(hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies", "cookieStore");
-        }
-        addListItem("Total: " + totalCookies + " cookies", "cookieStore");
-    },
-};
-
-pioneerSq.rndNumCustHr();
-
-var pdxAirport = {
-    storeLoc: "Portland Airport",
-    minCustHr: 6,
-    maxCustHr: 24,
-    avgCookiePerCust: 1.2,
-    rndNumCustHr: function() {
-        var totalCookies = 0;
-        var cookieArray = new Array();
-        addStoreName(pdxAirport.storeLoc, "cookieStoreLoc2");
-        for (var index = 0; index < 8; index++) {
-            var custNum = Math.floor(Math.random() * (pdxAirport.maxCustHr - pdxAirport.minCustHr) + pdxAirport.minCustHr);
-            var cookiesThatHr = custNum * pdxAirport.avgCookiePerCust;
-            cookieArray.push(cookiesThatHr);
-            totalCookies = totalCookies + Math.floor(cookiesThatHr);
-            addListItem(hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies", "cookieStore2");
-        }
-            addListItem("Total: " + totalCookies + " cookies", "cookieStore2");
-    },
-};
-
-pdxAirport.rndNumCustHr();
-
-var washSq = {
-    storeLoc: "Washington Square",
-    minCustHr: 11,
-    maxCustHr: 38,
-    avgCookiePerCust: 1.9,
-    rndNumCustHr: function() {
-        var totalCookies = 0;
-        var cookieArray = new Array();
-        addStoreName(washSq.storeLoc, "cookieStoreLoc3");
-        for (var index = 0; index < 8; index++) {
-            var custNum = Math.floor(Math.random() * (washSq.maxCustHr - washSq.minCustHr) + washSq.minCustHr);
-            var cookiesThatHr = custNum * washSq.avgCookiePerCust;
-            cookieArray.push(cookiesThatHr);
-            totalCookies = totalCookies + Math.floor(cookiesThatHr);
-            addListItem(hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies", "cookieStore3");
-        }
-            addListItem("Total: " + totalCookies + " cookies", "cookieStore3");
-    },
-};
-
-washSq.rndNumCustHr();
-
-var sellwood = {
-    storeLoc: "Sellwood",
-    minCustHr: 20, 
-    maxCustHr: 48,
-    avgCookiePerCust: 3.3,
-    rndNumCustHr: function() {
-        var totalCookies = 0;
-        var cookieArray = new Array();
-        addStoreName(sellwood.storeLoc, "cookieStoreLoc4");
-        for (var index = 0; index < 8; index++) {
-            var custNum = Math.floor(Math.random() * (sellwood.maxCustHr - sellwood.minCustHr) + sellwood.minCustHr);
-            var cookiesThatHr = custNum * sellwood.avgCookiePerCust;
-            cookieArray.push(cookiesThatHr);
-            totalCookies = totalCookies + Math.floor(cookiesThatHr);
-            addListItem(hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies", "cookieStore4");
-        }
-        addListItem("Total: " + totalCookies + " cookies", "cookieStore4");
-    },
-};  
-
-sellwood.rndNumCustHr();
-
-var pearlDis = {
-    storeLoc: "Pearl District",
-    minCustHr: 3,
-    maxCustHr: 24, 
-    avgCookiePerCust: 2.6,
-    rndNumCustHr: function() {
-        var totalCookies = 0;
-        var cookieArray = new Array();
-        addStoreName(pearlDis.storeLoc, "cookieStoreLoc5");
-        for (var index = 0; index < 8; index++) {
-            var custNum = Math.floor(Math.random() * (pearlDis.maxCustHr - pearlDis.minCustHr) + pearlDis.minCustHr);
-            var cookiesThatHr = custNum * pearlDis.avgCookiePerCust;
-            cookieArray.push(cookiesThatHr);
-            totalCookies = totalCookies + Math.floor(cookiesThatHr);
-            addListItem(hoursArray[index] + ": " + Math.floor(cookieArray[index]) + " cookies", "cookieStore5");
-        }
-        addListItem("Total: " + totalCookies + " cookies", "cookieStore5");
-    },
-};
-
-pearlDis.rndNumCustHr(); */
